15917eb4b9469d75fe7ddb084faec5ca14d30ea5
Clark <rclark010@gmail.com>
2019-03-06 11:46:44 -0700

    Ready for testing.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 // we'll need axios
+import axios from 'axios';
 
 // we'll need to create 3 different action types here.
 // one for fetching, one for success and one for failure
@@ -8,4 +9,17 @@ export const FAILURE = 'FAILURE';
 
 // our action creator will be a function that returns a function
 // the url to fetch characters from is `https://swapi.co/api/people/`
-// remember that now we have controll over our thunk-based action creator
+// remember that now we have control over our thunk-based action creator
+
+export const getCharacters = () => dispatch => {
+    // dispatch the FETCHING action
+    dispatch({type: FETCHING});
+    // initiate axios `get` call
+    axios
+        .get('https://swapi.co/api/people/')
+        .then(res =>
+            dispatch({type: SUCCESS, payload: res.data.results})
+        )
+        .catch(err => dispatch({type: FAILURE, payload: err}));
+};
+
--- src/views/CharacterListView.js
+++ src/views/CharacterListView.js
@@ -3,19 +3,21 @@ import { connect } from "react-redux";
 
 import { CharacterList } from "../components";
 // import actions
+import { getCharacters } from '../actions';
 
 class CharacterListView extends React.Component {
-  constructor() {
-    super();
-  }
 
   componentDidMount() {
     // call our action
+    this.props.getCharacters();
   }
 
   render() {
     if (this.props.fetching) {
       // return something here to indicate that you are fetching data
+      return (
+        <h2>Fetching Characters!</h2>
+      )
     }
     return (
       <div className="CharactersList_wrapper">
@@ -27,9 +29,17 @@ class CharacterListView extends React.Component {
 
 // our mapStateToProps needs to have two properties inherited from state
 // the characters and the fetching boolean
+const mapStateToProps = state => {
+  return {
+    characters: state.characters,
+    fetching: state.fetching
+  }
+}
+
 export default connect(
-  null /* mapStateToProps replaces null here */,
+  mapStateToProps,
   {
     /* action creators go here */
+    getCharacters
   }
 )(CharacterListView);
